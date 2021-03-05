2053a857ec73424717b6116569ac8a465a04ad99
MNM <moussanm2014@gmail.com>
2021-03-05 11:36:13 -0600

    Modified public/db.js file

--- public/db.js
+++ public/db.js
@@ -2,13 +2,13 @@ let db;
 // Creates a new db "myBudget" database :
 const request = indexedDB.open("myBudget", 1);
 
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     // object store called "pending" set autoIncrement to true :
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
     // checks if app is online before reading from db :
     if (navigator.onLine) {
@@ -17,7 +17,7 @@ request.onsuccess = function(event) {
 };
 
 // Error handling :
-request.onerror = function(event) {
+request.onerror = function (event) {
     console.log("Error" + event.target.errorCode)
 };
 
@@ -28,3 +28,29 @@ function saveRecord(record) {
     store.add(record);
 };
 
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    accept: "application/json, text/plain, */*",
+                    "content-type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+};
+// listen for app coming back online :
+window.addEventListener("online", checkDatabase);
